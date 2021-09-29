d078e278786be415d9b36eea1174e4b6fa2ebeb3
Basson Koch <bassonkoch970305@gmail.com>
2021-09-29 15:14:20 +0700

    Added method to check if ship was sunk

--- Battleships_v1.0.py
+++ Battleships_v1.0.py
@@ -151,11 +151,24 @@ def accept_valid_bullet_placement():
         if board[row][col] == "." or board[row][col] == "O":
             no_errors_with_shot = True
             
-    return row, col
-        
+    return row, col        
 
 def check_for_ship_sunk(row, col):
-    pass
+    
+    global board
+    global ship_positions
+    
+    for position in ship_positions:
+        start_row = position[0]
+        end_row = position[1]
+        start_col = position[2]
+        end_col = position[3]
+        if start_row <= row <= end_row and start_col <= col <= end_col:
+            for r in range(start_row, end_row):
+                for c in range(start_col, end_col):
+                    if board[r][c] != "X":
+                        return False
+    return True
 
 def shoot_target():
     
@@ -181,6 +194,8 @@ def shoot_target():
             print("You shot a ship")
     
     ammo -= 1
+
+
 
 def main():
     
