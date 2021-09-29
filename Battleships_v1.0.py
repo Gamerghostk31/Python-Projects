ba00253953bd25b72939f07e4a1dd0ed5bfbe588
Basson Koch <bassonkoch970305@gmail.com>
2021-09-29 12:10:58 +0700

    Created method for creating a board and added code for testing

--- Battleships_v1.0.py
+++ Battleships_v1.0.py
@@ -16,22 +16,46 @@ num_ships_sunk = 0
 game_over = False
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def create_board():
+    
+    global board
+    global board_size
+    global num_of_ships
+    global ship_positions
+    
+    random.seed(time.time())
+    
+    # Set length of rows and cols to the board size (10)
+    rows, cols = (board_size, board_size)
+    
+    board = []
+    # Vertical Rows
+    for r in range(rows):
+        row = []
+        # Horzontal Columns
+        for c in range(cols):
+            row.append(".")
+        board.append(row)
 
+    print(board)
 
 def main():
     
     print("""
-          Welcome to...          
-  _                                    
- |_)  _. _|_ _|_ |  _   _ |_  o ._   _ 
- |_) (_|  |_  |_ | (/_ _> | | | |_) _> 
-                                |      
-________________________________________
-By Basson Koch
-________________________________________
-          """)
+                    Welcome to...          
+          _                                    
+         |_)  _. _|_ _|_ |  _   _ |_  o ._   _ 
+         |_) (_|  |_  |_ | (/_ _> | | | |_) _> 
+                                        |      
+        ________________________________________
+                    By Basson Koch
+        ________________________________________
+           """)
     
     print("You have 50 rounds of ammo to sink 6 ships... Goodluck!")
+    
+    create_board()
+    
     
     
     
