ff99c679cd8d0eb3b9a809908f352d6dcc350e0b
Basson Koch <bassonkoch970305@gmail.com>
2021-10-03 11:32:43 +0700

    Added print method for printing the grid and skipping changed grid positions

--- Personal Portfolio Projects/Battleships/Battleships_v2.0.py
+++ Personal Portfolio Projects/Battleships/Battleships_v2.0.py
@@ -10,6 +10,7 @@ ships_sunk = 0
 ship_positions = []
 ammo = 0
 game_over = False
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 # Methods Needed
 def create_grid():
@@ -17,21 +18,43 @@ def create_grid():
     global grid
     global grid_size
     
-    rows, cols = grid_size, grid_size
+    rows, cols = (grid_size, grid_size)
     
-    for row in rows:
+    for row in range(rows):
         row_list = []
-        for col in cols:
+        for col in range(cols):
             row_list.append(".")
         grid.append(row_list)
-            
-    
-
+              
 def create_ship_positions():
-    pass
-
+    
+    random.seed(time.time())
+    
 def print_grid():
-    pass
+
+    global grid
+    global alphabet
+    
+    alphabet = alphabet[0: len(grid) + 1]
+    
+    for row in range(len(grid)):
+        print(alphabet[row], end=")  ")
+        for col in range(len(grid[row])):
+            if grid[row][col] == "X":
+                pass
+            elif grid[row][col] == "#":
+                pass
+            else:
+                print(".", end=" ")
+        print("")
+    
+    print("  ", end=" ")
+    for i in range(len(grid[0])):
+        print(str(i), end=" ")
+    print("")
+    
+    
+    
 
 def check_ships_in_range():
     pass
@@ -50,7 +73,8 @@ def check_game_over():
 
 # Main Method
 def main():
-    pass
+    create_grid()
+    print_grid()
 
 if __name__ == '__main__':
     main()
