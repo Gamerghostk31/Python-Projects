cd08f0587068cf21a2a2d7fdf9aee1c5f62a05ac
Basson Koch <bassonkoch970305@gmail.com>
2021-10-03 10:59:40 +0700

    Added placeholder methods and global variable based on project needs

---
+++ Personal Portfolio Projects/Battleships/Battleships_v2.0.py
@@ -0,0 +1,38 @@
+import random
+import time
+
+# Global Variables
+grid = []
+grid_size = 10
+num_of_ships = 8
+ships_sunk = 0
+ship_positions = []
+ammo = 0
+game_over = False
+
+def create_grid():
+    pass
+
+def create_ship_positions():
+    pass
+
+def check_ships_in_range():
+    pass
+
+def check_ship_collision():
+    pass
+
+def shoot():
+    pass
+
+def check_ship_hit():
+    pass
+
+def check_game_over():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
