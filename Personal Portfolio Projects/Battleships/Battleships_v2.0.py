2a223b189a12c0b6e24d3bce335c19e8c5792e64
Basson Koch <bassonkoch970305@gmail.com>
2021-10-03 11:52:27 +0700

    Reworked creating ship positions and validation methods

--- Personal Portfolio Projects/Battleships/Battleships_v2.0.py
+++ Personal Portfolio Projects/Battleships/Battleships_v2.0.py
@@ -30,6 +30,23 @@ def create_ship_positions():
     
     random.seed(time.time())
     
+    global grid
+    global grid_size
+    global num_of_ships
+    global ship_positions
+    
+    rows, cols = (grid_size, grid_size)
+    
+    ships_placed = 0
+    
+    while ships_placed != num_of_ships:
+        random_row = random.randint(0, rows - 1)
+        random_col = random.randint(0, cols - 1)
+        direction = random.choice(["left", "right", "up", "down"])
+        random_size = random.randint(2, 5)
+        if check_ships_in_range(random_row, random_col, direction, random_size):
+            ships_placed += 1
+    
 def print_grid():
 
     global grid
@@ -56,11 +73,50 @@ def print_grid():
         print(str(i), end=" ")
     print("")
           
-def check_ships_in_range():
-    pass
+def check_ships_in_range(row, col, direction, size):
+    
+    global grid_size
 
-def check_ship_collision():
-    pass
+    start_row, end_row, start_col, end_col = row, row + 1, col, col + 1
+    if direction == "left":
+        if col - size < 0:
+            return False
+        start_col = col - size + 1
+
+    elif direction == "right":
+        if col + size >= grid_size:
+            return False
+        end_col = col + size
+
+    elif direction == "up":
+        if row - size < 0:
+            return False
+        start_row = row - size + 1
+
+    elif direction == "down":
+        if row + size >= grid_size:
+            return False
+        end_row = row + size
+
+    return add_ships(start_row, end_row, start_col, end_col)
+
+def add_ships(start_row, end_row, start_col, end_col):
+    
+    global grid
+    global ship_positions
+
+    all_valid = True
+    for r in range(start_row, end_row):
+        for c in range(start_col, end_col):
+            if grid[r][c] != ".":
+                all_valid = False
+                break
+    if all_valid:
+        ship_positions.append([start_row, end_row, start_col, end_col])
+        for r in range(start_row, end_row):
+            for c in range(start_col, end_col):
+                grid[r][c] = "O"
+    return all_valid
 
 def shoot():
     pass
@@ -74,6 +130,7 @@ def check_game_over():
 # Main Method
 def main():
     create_grid()
+    create_ship_positions()
     print_grid()
 
 if __name__ == '__main__':
