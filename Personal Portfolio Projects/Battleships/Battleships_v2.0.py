55666a3cd8bb58d78a17f2cb5d46b5da73705f95
Basson Koch <bassonkoch970305@gmail.com>
2021-10-03 11:58:38 +0700

    Added methods for shooting, error handling and intro

--- Personal Portfolio Projects/Battleships/Battleships_v2.0.py
+++ Personal Portfolio Projects/Battleships/Battleships_v2.0.py
@@ -8,7 +8,7 @@ grid_size = 10
 num_of_ships = 8
 ships_sunk = 0
 ship_positions = []
-ammo = 0
+ammo = 50
 game_over = False
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
@@ -119,19 +119,126 @@ def add_ships(start_row, end_row, start_col, end_col):
     return all_valid
 
 def shoot():
-    pass
+    
+    global grid
+    global ships_sunk
+    global ammo
+    
+    row, col = bullet_placement()
+    
+    print("")
+    print("_____________________________")
+    
+    if grid[row][col] == ".":
+        print("You missed, no ship was hit...")
+        grid[row][col] = "#"
+    elif grid[row][col] == "O":
+        print("You hit!", end=" ")
+        grid[row][col] = "X"
+        if check_ship_hit(row, col):
+            print("You completely sunk a ship!")
+            ships_sunk += 1
+        else:
+            print("You shot a ship")
+    
+    ammo -= 1
 
-def check_ship_hit():
-    pass
+def bullet_placement():
+    global alphabet
+    global grid
+    
+    no_errors_with_shot = False
+    row = -1
+    col = -1
+    while no_errors_with_shot is False:
+        placement = input("Enter a row and column to attack, like \"B5\":    ")
+        placement = placement.upper()
+        if len(placement) <= 0 or len(placement) > 2:
+            print("Error: Please enter only one row and column")
+            continue
+        row = placement[0]
+        col = placement[1]
+        if not row.isalpha() or not col.isnumeric():
+            print("Error: Please enter a letter and then a number...")
+            continue
+        row = alphabet.find(row)
+        if not (-1 < row < grid_size):
+            print("Error: Please enter a letter and then a number...")
+            continue
+        col = int(col)
+        if not (-1 < col < grid_size):
+            print("Error: Please enter a letter and then a number...")
+            continue
+        if grid[row][col] == "#" or grid[row][col] == "X":
+            print("You've already attacked this position...")
+            continue
+        if grid[row][col] == "." or grid[row][col] == "O":
+            no_errors_with_shot = True
+            
+    return row, col   
+
+def check_ship_hit(row, col):
+    global grid
+    global ship_positions
+    
+    for position in ship_positions:
+        start_row = position[0]
+        end_row = position[1]
+        start_col = position[2]
+        end_col = position[3]
+        if start_row <= row <= end_row and start_col <= col <= end_col:
+            for r in range(start_row, end_row):
+                for c in range(start_col, end_col):
+                    if grid[r][c] != "X":
+                        return False
+    return True
 
 def check_game_over():
-    pass
+    
+    global game_over
+    global ammo
+    global num_of_ships
+    global ships_sunk
+    
+    if num_of_ships == ships_sunk:
+        print("YOU WIN! CONGRATULATIONS!!!")
+        game_over = True
+    elif  ammo <= 0:
+        print("Sorry, you're out of ammo... GAME OVER")
+        game_over = True
 
 # Main Method
 def main():
+    
+    global game_over
+    global num_of_ships
+    global ships_sunk
+    global ammo
+    
+    print("""
+                    Welcome to...          
+          _                                    
+         |_)  _. _|_ _|_ |  _   _ |_  o ._   _ 
+         |_) (_|  |_  |_ | (/_ _> | | | |_) _> 
+                                        |      
+        ________________________________________
+                    By Basson Koch
+        ________________________________________
+           """)
+    
+    print("You have 50 rounds of ammo to sink 6 ships... Goodluck!")
+    
     create_grid()
     create_ship_positions()
-    print_grid()
+    
+    while game_over is False:
+        print_grid()
+        print("There are {ships} ships remaining, You have sunk {ships_sunk} ships".format(ships=num_of_ships, ships_sunk=num_ships_sunk))
+        print("You have {ammo} rounds of ammo remaining".format(ammo=ammo))
+        shoot()
+        print("________________________________")
+        print("")
+        check_game_over()
 
 if __name__ == '__main__':
     main()
