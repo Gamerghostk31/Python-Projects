5cfd6a3ddaff6f5f7f7f58785d7cec9636aeb4ea
Basson Koch <bassonkoch970305@gmail.com>
2021-10-16 14:35:44 +0700

    Added class object for number generation, empty method for checking game over and also imported random and time to create a random number within the ranges provided

--- Personal Portfolio Projects/Number Guessing Game/Number Guessing Game.py
+++ Personal Portfolio Projects/Number Guessing Game/Number Guessing Game.py
@@ -4,3 +4,33 @@
 # to select a difficulty. Normal (1 - 10) and Hard (1 - 100) the computer will then choose a a number in that range and if 
 # the difficulty is set on hard it will give a clue. For every wrong guess, points are deducted. The aim is to guess the number before your score reaches 0
 # The score will be saved in a text file and display the highest score for each difficulty on the next game. 
+
+from random import randint, random
+from datetime import time
+
+
+
+class number_generator():
+    
+    global random
+    
+    def __init__(self, game_difficulty):
+        self.game_difficulty = game_difficulty
+
+    def setGameDifficulty(self):
+        
+        if self.game_difficulty == "hard":
+            self.number = random.randint(1, 100)
+            return self.number
+        elif self.game_difficulty == "normal":
+            self.number = random.randint(1, 10)
+            return self.number
+
+def check_game_over():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
