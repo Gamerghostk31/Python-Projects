7a2d8d42afb39c90b6d05153d2dfee3875eb7a51
Basson Koch <bassonkoch970305@gmail.com>
2021-10-16 14:24:43 +0700

    Added Readme info in the top description of code

---
+++ Personal Portfolio Projects/Number Guessing Game/Number Guessing Game.py
@@ -0,0 +1,6 @@
+# Number Guessing Game
+# Author & Date: Basson Koch | 16/10/2021
+# The player will start out with 100 points, after which the computer will prompt the player
+# to select a difficulty. Normal (1 - 10) and Hard (1 - 100) the computer will then choose a a number in that range and if 
+# the difficulty is set on hard it will give a clue. For every wrong guess, points are deducted. The aim is to guess the number before your score reaches 0
+# The score will be saved in a text file and display the highest score for each difficulty on the next game. 
